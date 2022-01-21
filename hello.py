6c789ace29498ade0b854ce7798b687e982d5133
unknown <arcrilest@outlook.com>
2022-01-21 07:05:19 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,30 @@
 from tkinter import *
 
 root = Tk()
+input = Entry(root)
+
+input.pack()
+
 
 myLabel = Label(root, text="Hello World!")
 
-myLabel.pack()
 
+Sortbutton = Button(root, text="Sort!", padx=50)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+myLabel.pack()
+Sortbutton.pack()
 root.mainloop()
 
